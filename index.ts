d888723757cc6d49366077c0f60f2c66458f8e8b
Lam Kee Wei <keewei.lam@gmail.com>
2022-11-07 13:57:38 +0800

    chore: add linting to files (#5)
    * chore: configure linting
    
    * chore: lint and fix files
    
    * chore: add trailing newline

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
-import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
+import { GithubOidc, PulumiS3States } from '@opengovsg/pulumi-helpers'
+import * as aws from '@pulumi/aws'
+import * as pulumi from '@pulumi/pulumi'
 
-import { GithubOidc, PulumiS3States } from "@opengovsg/pulumi-helpers";
+const github = new GithubOidc('sk-infra', {
+  repo: 'opengovsg/starter-kit-infra',
+})
 
-const github = new GithubOidc("sk-infra", {
-  repo: "opengovsg/starter-kit-infra",
-});
+export const githubOidcRoleArn = github.role.arn
 
-export const githubOidcRoleArn = github.role.arn;
+const pulumiS3States = new PulumiS3States('sk-infra', {})
 
-const pulumiS3States = new PulumiS3States("sk-infra", {});
-
-export const s3Uri = pulumiS3States.s3Uri;
-export const keyUri = pulumiS3States.keyUri;
+export const s3Uri = pulumiS3States.s3Uri
+export const keyUri = pulumiS3States.keyUri
