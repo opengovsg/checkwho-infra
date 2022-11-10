c9228c77c5ffbc5c8b16a659d603fe8e03081e42
Blake Y. Gong <blake@open.gov.sg>
2022-11-10 13:48:26 +0800

    chore: bump pulumi components (#7)

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-import { GithubOidc, PulumiS3States } from '@opengovsg/pulumi-helpers'
+import { GithubOidc, PulumiS3States } from '@opengovsg/pulumi-components'
 import * as aws from '@pulumi/aws'
 import * as pulumi from '@pulumi/pulumi'
