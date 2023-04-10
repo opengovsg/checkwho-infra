9e0f91550a661917ce60b69518040b2f2b4d6c02
zx <67887489+tan-zx@users.noreply.github.com>
2023-04-10 19:40:15 +0800

    chore: initial commit

---
+++ docs/first-time-setup.md
@@ -0,0 +1,83 @@
+# First time setup
+
+For now, there are some manual steps involved to get this repository up and running. They will be moved into scripts eventually.
+
+## Prepare names
+
+Please prepare the names of your project and team. For this guide, they will be referred to:
+
+- `checkwho-infra`
+- `checkwho` (a shorter version (less than 10 characters) of your project name. If the project name is short enough, this can be the same as the project name)
+- `checkwho-team`
+
+**To speed up the process, copy this page to your editor, and replace all the `checkwho-infra`, `checkwho` and `checkwho-team` to the real values you have, so that subsequently the commands can be copied and executed directly.**
+
+## Check AWS access
+
+For the first time setup, you will need programmatic access to your AWS account.
+
+```bash
+aws sts get-caller-identify
+# check if it is the correct account
+```
+
+## Install dependencies
+
+```bash
+brew install pulumi
+
+git clone git@github.com:opengovsg/starter-kit-infra checkwho-infra
+cd checkwho-infra
+
+git grep -l 'starter-kit-infra' | grep -v '.github' | xargs sed -i '' -e 's/starter-kit-infra/checkwho-infra/g'
+git grep -l 'sk-infra' | grep -v '.github' | xargs sed -i '' -e 's/sk-infra/checkwho/g'
+git grep -l 'starter-kit-team' | grep -v '.github' | xargs sed -i '' -e 's/starter-kit-team/checkwho-team/g'
+npm i
+
+export PULUMI_CONFIG_PASSPHRASE=''
+pulumi login file://~
+mv Pulumi.sk-infra.staging.yaml Pulumi.checkwho.staging.yaml
+pulumi stack init checkwho.staging
+
+pulumi up
+# select `yes` if all the resources seem reasonable
+
+githubOidcRoleArn=$(pulumi stack output githubOidcRoleArn)
+s3Uri=$(pulumi stack output s3Uri)
+git grep -l 'arn:aws:iam::075109709139:role/sk-infra-github-oidc-role-e6eab36' | xargs sed -i '' -e 's,arn:aws:iam::075109709139:role/sk-infra-github-oidc-role-e6eab36,'"$githubOidcRoleArn"',g'
+git grep -l 's3://sk-infra-pulumi-s3-states-bucket-0f5edcd' | xargs sed -i '' -e 's,s3://sk-infra-pulumi-s3-states-bucket-0f5edcd,'"$s3Uri"',g'
+git grep -l 'sk-infra.staging' | xargs sed -i '' -e 's/sk-infra.staging/checkwho.staging/g'
+
+
+pulumi stack export --show-secrets --file stack.json
+# make sure `stack.json` gets created which means the export above has been successful!
+pulumi stack rm checkwho.staging -f --preserve-config -y
+pulumi logout
+pulumi login $s3Uri
+pulumi stack init checkwho.staging
+pulumi stack import --file stack.json
+# make sure the import above has been successful!
+rm stack.json
+
+pulumi stack change-secrets-provider "$(pulumi stack output keyUri)"
+```
+
+Verify things have been migrated correctly!
+
+```bash
+pulumi up
+```
+
+You should see "x unchanged".
+
+You can push the code to Github now and continuous deployment has already been set up! If the changes are made against `develop` or `staging` branch, you should see something similar to https://github.com/opengovsg/starter-kit-infra/pull/3.
+
+```bash
+rm -rf .git # clean up carried over git history
+git init
+git checkout -b develop
+git add .
+git commit -m "chore: initial commit"
+git remote add origin git@github.com:opengovsg/checkwho-infra.git
+git push -u origin develop
+```
---
+++ index.ts
@@ -0,0 +1,120 @@
+import {
+  Bastion,
+  CfValidatedCert,
+  Ecs,
+  loadAwsProviderDefaultTags,
+  Rds,
+  SecurityGroupConnection,
+  SHORT_ENV_MAP,
+  SsmParams,
+  Vpc,
+} from '@opengovsg/pulumi-components'
+import * as aws from '@pulumi/aws'
+import * as pulumi from '@pulumi/pulumi'
+
+export const { env, project, team } = loadAwsProviderDefaultTags()
+export const shortEnv = SHORT_ENV_MAP[env]
+
+// Having env in every name makes multiple env (e.g. prod + stg) in one AWS account possible
+const name = `checkwho-${shortEnv}`
+const isProd = shortEnv === 'prod'
+
+const cfValidatedCert = new CfValidatedCert(name, {
+  cfZoneId: '7f702ea2e13fb4f9cd204be342e080c0',
+  domainName: isProd ? 'checkwho.gov.sg' : 'staging.checkwho.gov.sg',
+})
+
+// ======================================== VPC =========================================
+const vpc = new Vpc(name, {
+  isProd,
+  secondOctet: 16,
+  // enableS3GatewayVpcEndpoint: true,
+})
+
+// ========================== ECS (including LB) + CF/ACM cert ==========================
+const ecr = new aws.ecr.Repository(
+  name,
+  {
+    name,
+    // TODO: (temporary) enable forceDelete to make teardown easier
+    forceDelete: true,
+  },
+  {
+    // must delete before replace, otherwise the specified ECR name above will cause conflict
+    deleteBeforeReplace: true,
+  },
+)
+export const ecrUri = ecr.repositoryUrl
+
+const ecs = new Ecs(
+  name,
+  {
+    loadBalancingArgs: {
+      allowCloudFlareOriginatedTraffic: true,
+      allowOgpVpnOriginatedTraffic: true,
+      httpsCertificateArn: cfValidatedCert.certificate.arn,
+    },
+    vpc,
+  },
+  {
+    // Unfortunately dependency in Pulumi doesn't work at the ComponentResource level.
+    // We have to manually state child resource dependency when needed.
+    // This particular dependency makes sure that our ALB will only be created after the HTTPS cert
+    // has been validated, otherwise the cert cannot be issued thus cannot be attached to the ALB.
+    dependsOn: cfValidatedCert.validation,
+  },
+)
+export const lbUrl = pulumi.interpolate`${ecs.loadBalancer.dnsName}`
+
+// ======================================== RDS =========================================
+const rds = new Rds(name, {
+  // TODO: (temporary) use dangerouslyPrepareForDeletion to make teardown easier
+  dangerouslyPrepareForDeletion: true,
+  isProd,
+  vpc,
+})
+const allowEcsTaskToRds = new SecurityGroupConnection(
+  `${name}-ecs-task-to-rds`,
+  {
+    description: 'Allow traffic from ECS Task to RDS',
+    fromSg: ecs.taskSecurityGroup,
+    toSg: rds.securityGroup,
+    port: 5432,
+  },
+)
+
+// ======================================= Bastion ======================================
+const publicKey =
+  shortEnv === 'prod'
+    ? 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZiy7lp0dDuKE1o7I0mSbPMgMaBp5y2NZM7YObJGDC7'
+    : 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMi6eFyN4+Hw3tQHSLjOxaijUQ6LbF2l+/4/8ir5LOgV'
+const bastion = new Bastion(name, { vpc, publicKey })
+export const bastionSshCommand = bastion.sshCommand
+
+const allowBastionToRds = new SecurityGroupConnection(
+  `${name}-bastion-to-rds`,
+  {
+    description: 'Allow traffic from Bastion EC2 to RDS',
+    fromSg: bastion.securityGroup,
+    toSg: rds.securityGroup,
+    port: 5432,
+  },
+)
+
+const params = new SsmParams(name, {
+  env: shortEnv,
+  prefix: `${shortEnv}/checkwho`,
+  specs: [
+    // DB
+    { key: 'DB_HOST', value: rds.secrets.endpoint },
+    { key: 'DB_HOST_REPLICA', value: rds.secrets.readerEndpoint },
+    { key: 'DB_NAME', value: rds.secrets.database },
+    {
+      key: 'DB_PASSWORD',
+      secure: true,
+      value: rds.secrets.password,
+    },
+    { key: 'DB_PORT', value: rds.secrets.port },
+    { key: 'DB_USERNAME', value: rds.secrets.username },
+  ],
+})
